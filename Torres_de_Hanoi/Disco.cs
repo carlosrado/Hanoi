9463953af9583ea4116e9a02f5e1c463edfd7f57
Carlos <carlrd99@gmail.com>
2021-04-23 19:42:45 +0200

    Commit  Clase Disco

--- Torres_de_Hanoi/Disco.cs
+++ Torres_de_Hanoi/Disco.cs
@@ -8,11 +8,22 @@ namespace Torres_de_Hanoi
 {
     class Disco
     {
-        /*TODO: 
-        Decidir tipo de Valor
         public int Valor { get; set; }
-        public String Valor { get; set; }
-        */
 
+
+        public Disco(int valor)
+        {
+            Valor = valor;
+        }
+
+        public bool mayorQue(Disco disco)
+        {
+            if (disco == null) { 
+                return true; 
+            }
+            else {
+                return Valor > disco.Valor; 
+            }
+        }
     }
 }
