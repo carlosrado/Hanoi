6cccfabaef2502bbb7be257aa98c0e91768a3e82
Carlos <carlrd99@gmail.com>
2021-04-25 14:06:44 +0200

    Clase Hanoi

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,16 +8,48 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
-        /*TODO: Implementar métodos*/
+        private int m = 0;
+
         public void mover_disco(Pila a, Pila b)
         {
+            //si a < b, muevo de a hacia b. En caso contrario, de b hacia a.
+            if(a.Top < b.Top && !a.isEmpty() || b.isEmpty())
+            {
+                b.push(a.pop());
+                Console.Write("Movemos el disco de tamaño: " + a.Top + "encima del disco de tamaño" + b.Top);
+            } else
+            {
+                a.push(b.pop());
+                Console.Write("Movemos el disco de tamaño: " + b.Top + "encima del disco de tamaño" + a.Top);
 
+            }
         }
-
-        public int iterativo(int n, Pila ini, Pila fin, Pila aux)
+        public int iterativo(Pila ini, Pila aux, Pila fin)
         {
-            return 0;
+            int m = 0;
+            if (n % 2 == 1)
+            {
+                while (fin.Size != n)
+                {
+                    mover_disco(ini, fin);
+                    m++;
+                    mover_disco(ini, aux);
+                    m++;
+                    mover_disco(aux, fin);
+                    m++;
+                }
+            } else if (n % 2 == 0)
+            {
+                while (fin.Size != n)
+                {
+                    mover_disco(ini, aux);
+                    m++;
+                    mover_disco(ini, fin);
+                    m++;
+                    mover_disco(aux, fin);
+                    m++;
+                }
+            }
+            return m;
         }
-
-    }
 }
